3d011f02140bca9a2fa94dd05274bdd19ffb7ef4
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-04 14:33:29 +0200

    complete task

---
+++ src/api/handleCompleteTask.ts
@@ -0,0 +1,42 @@
+import { SERVER_ADDRESS } from '../constants/constants'
+import type { handleCompleteTaskProps, TaskProps } from '../types/types'
+import { setItemInSessionStorage } from '../utils/setItemInSessionStorage'
+
+export const handleCompleteTask = async ({
+    setUpcomingTasks,
+    showToast,
+    task,
+    upcomingTasks,
+}: handleCompleteTaskProps) => {
+    try {
+        const response = await fetch(
+            `${SERVER_ADDRESS}/api/tasks/${task.id}/`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ finished: true }),
+            }
+        )
+
+        if (!response.ok) {
+            showToast({
+                isSuccess: false,
+                label: 'Aufgaben konnten nicht erledigt werden.',
+            })
+            return
+        }
+
+        const updatedTasks = upcomingTasks.filter(
+            (item: TaskProps) => item.id !== task.id
+        )
+        setUpcomingTasks(updatedTasks)
+        setItemInSessionStorage('upcomingTasks', updatedTasks)
+    } catch {
+        showToast({
+            isSuccess: false,
+            label: 'Beim Erledigen dieser Aufgabe ist ein Fehler aufgetreten.',
+        })
+    }
+}
