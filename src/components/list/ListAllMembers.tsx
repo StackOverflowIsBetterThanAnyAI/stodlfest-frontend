d6dada7d95e09f63fbee62809da567a1238fcc84
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-08 16:46:58 +0200

    list all available members

--- src/components/list/ListAllMembers.tsx
+++ src/components/list/ListAllMembers.tsx
@@ -5,6 +5,7 @@ import ListNoItems from './ListNoItems'
 import { AllMembersContext } from '../../context/AllMembersContext'
 import { useToast } from '../../context/ToastContext'
 import { handleFetchAllMembers } from '../../api/handleFetchAllMembers'
+import type { MemberProps } from '../../types/types'
 
 const ListAllMembers = () => {
     const { showToast } = useToast()
@@ -36,7 +37,31 @@ const ListAllMembers = () => {
                 label="Neuladen"
                 type="refresh"
             />
-            {allMembers ? undefined : (
+            {allMembers?.length ? (
+                <ul className="flex flex-col gap-4 mx-2">
+                    {allMembers.map((member: MemberProps, index: number) => (
+                        <li
+                            key={member.id}
+                            className={`flex flex-col ${index < allMembers.length - 1 ? 'pb-4 border-b-2' : ''} border-zinc-200`}
+                        >
+                            <div
+                                className={`py-2 px-3 flex flex-col gap-2 ${index % 2 ? 'bg-slate-800' : 'bg-slate-700'} rounded-sm`}
+                            >
+                                <div className="flex flex-wrap gap-2 justify-between items-center">
+                                    <span className="text-base md:text-lg">
+                                        {member.name} {member.surname}
+                                    </span>
+                                    <em className="text-sm md:text-base">
+                                        {member.age === 'ofLegalAge'
+                                            ? 'Volljährig'
+                                            : 'Minderjährig'}
+                                    </em>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
                 <ListNoItems label="Es sind noch keine Mitglieder vorhanden." />
             )}
         </section>
