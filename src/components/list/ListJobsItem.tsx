f467664c92f84d69630befc7101526bbcc2d6dea
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-10 17:13:02 +0200

    erforderliche Helfer

--- src/components/list/ListJobsItem.tsx
+++ src/components/list/ListJobsItem.tsx
@@ -5,12 +5,12 @@ const ListJobsItem = ({ index, job }: ListJobsItemProps) => {
         <div
             className={`py-2 px-3 flex flex-col gap-2 ${index % 2 ? 'bg-slate-800' : 'bg-slate-700'} rounded-sm`}
         >
-            <div className="flex flex-wrap gap-x-4 justify-between items-center">
-                <span className="flex flex-col">
-                    <span className="text-base md:text-lg">{job.job}</span>
-                    <span className="text-base md:text-lg">{job.workers}</span>
+            <span className="flex flex-wrap gap-4 justify-between">
+                <span className="text-base md:text-lg">{job.job}</span>
+                <span className="text-base md:text-lg">
+                    {job.workers} erforderliche Helfer
                 </span>
-            </div>
+            </span>
         </div>
     )
 }
