5d77753b864697466fa3ee5bd1ec30f11904eb92
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-16 14:47:51 +0200

    explicitly apply type button

--- src/components/list/ListButton.tsx
+++ src/components/list/ListButton.tsx
@@ -18,7 +18,7 @@ const ListButton = ({
                 </div>
             ) : (
                 <button
-                    type={isSubmit ? 'submit' : undefined}
+                    type={isSubmit ? 'submit' : 'button'}
                     onClick={handleClick}
                     disabled={isDisabled || isLoading}
                     className="primary-text-pseudo text-sm md:text-base rounded-lg outline-2 outline-zinc-500 max-w-32 w-full h-8 md:h-10 px-4 py-1 mx-auto"
@@ -34,7 +34,7 @@ const ListButton = ({
                 </div>
             ) : (
                 <button
-                    type={isSubmit ? 'submit' : undefined}
+                    type={isSubmit ? 'submit' : 'button'}
                     onClick={handleClick}
                     disabled={isDisabled || isLoading}
                     className={`${index % 2 ? 'primary-text-pseudo-secondary' : 'primary-text-pseudo'} h-7 md:h-8 text-sm md:text-base rounded-lg outline-2 outline-zinc-500 max-w-72 w-full px-4 py-1 mx-auto`}
@@ -49,7 +49,7 @@ const ListButton = ({
             </div>
         ) : (
             <button
-                type={isSubmit ? 'submit' : undefined}
+                type={isSubmit ? 'submit' : 'button'}
                 onClick={handleClick}
                 disabled={isDisabled || isLoading}
                 className="primary-text-pseudo outline-2 outline-zinc-500 px-2 text-base md:text-lg h-8 md:h-10"
