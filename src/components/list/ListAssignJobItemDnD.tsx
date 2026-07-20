ba37aaa8e9b8a30e8248456dfa78526856ee0f0b
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-20 15:44:30 +0200

    divide u18 and 18+

--- src/components/list/ListAssignJobItemDnD.tsx
+++ src/components/list/ListAssignJobItemDnD.tsx
@@ -39,12 +39,14 @@ const ListAssignJobItemDnD = ({
         setActiveTargetZone(null)
     }
 
-    const handleDragOver = (e: React.DragEvent<HTMLUListElement>) => {
+    const handleDragOver = (
+        e: React.DragEvent<HTMLDivElement | HTMLUListElement>
+    ) => {
         e.preventDefault()
     }
 
     const handleDrop = async (
-        e: React.DragEvent<HTMLUListElement>,
+        e: React.DragEvent<HTMLDivElement | HTMLUListElement>,
         targetAction: TargetActionType
     ) => {
         e.preventDefault()
@@ -121,49 +123,88 @@ const ListAssignJobItemDnD = ({
         })
     }
 
+    const { underAgedMembersNoJob, ofLegalAgeMembersNoJob } =
+        allMembers?.reduce(
+            (total, cur: MemberProps) => {
+                if (!cur.job?.length) {
+                    if (
+                        job.requires_legal_age === 'doesNotRequireLegalAge' &&
+                        cur.age === 'underage'
+                    ) {
+                        total.underAgedMembersNoJob.push(cur)
+                    } else if (cur.age === 'ofLegalAge') {
+                        total.ofLegalAgeMembersNoJob.push(cur)
+                    }
+                }
+                return total
+            },
+            {
+                underAgedMembersNoJob: [] as MemberProps[],
+                ofLegalAgeMembersNoJob: [] as MemberProps[],
+            }
+        ) || { underAgedMembersNoJob: [], ofLegalAgeMembersNoJob: [] }
+
     return isToBeAssigned ? (
-        <ul
-            className={`flex flex-wrap gap-x-4 gap-y-2 items-start content-start overflow-y-auto h-32 p-2 flex-1 min-w-44 xs:min-w-64 rounded-md
-                                ${activeTargetZone === 'unassign' ? 'outline-dashed outline-2 outline-slate-200 bg-slate-200 animate-pulse' : 'bg-slate-400'}`}
+        <div
+            className={`flex flex-wrap ${underAgedMembersNoJob?.length && ofLegalAgeMembersNoJob?.length ? 'gap-x-4 gap-y-2' : ''} items-start content-start overflow-y-auto h-32 p-2 flex-1 min-w-44 xs:min-w-64 rounded-md
+                ${activeTargetZone === 'unassign' ? 'outline-dashed outline-2 outline-slate-200 bg-slate-200 animate-pulse' : 'bg-slate-400'}`}
             onDragOver={handleDragOver}
             onDrop={(e) => handleDrop(e, 'unassign')}
             aria-label="Verfügbare Mitglieder"
         >
-            {allMembers?.length
-                ? allMembers
-                      ?.filter((member) => {
-                          const hasNoJob = !member.job?.length
-                          const meetsAgeRequirement =
-                              job.requires_legal_age !==
-                                  'doesRequireLegalAge' ||
-                              member.age === 'ofLegalAge'
-                          return hasNoJob && meetsAgeRequirement
-                      })
-                      .map((member) => (
-                          <li
-                              key={member.id}
-                              draggable={currentWorkersCount !== job.workers}
-                              onDragStart={(e) => handleDragStart(e, member.id)}
-                              onDragEnd={handleDragEnd}
-                          >
-                              <button
-                                  disabled={currentWorkersCount === job.workers}
-                                  onKeyDown={(e) =>
-                                      handleKeyDown(e, member, 'assign')
-                                  }
-                                  className={`primary-text-pseudo-tertiary bg-slate-800 text-white px-2 py-1 text-sm md:text-base select-none 
+            {underAgedMembersNoJob?.length ? (
+                <ul
+                    className={`flex flex-wrap gap-x-4 gap-y-2 items-start content-start ${ofLegalAgeMembersNoJob?.length ? 'border-b-2 border-zinc-200/75 pb-2 w-full' : ''}`}
+                >
+                    {underAgedMembersNoJob.map((member) => (
+                        <li
+                            key={member.id}
+                            draggable={currentWorkersCount !== job.workers}
+                            onDragStart={(e) => handleDragStart(e, member.id)}
+                            onDragEnd={handleDragEnd}
+                        >
+                            <button
+                                disabled={currentWorkersCount === job.workers}
+                                onKeyDown={(e) =>
+                                    handleKeyDown(e, member, 'assign')
+                                }
+                                className={`primary-text-pseudo-tertiary bg-slate-800 text-white px-2 py-1 text-sm md:text-base select-none 
                                                 ${currentWorkersCount === job.workers ? '' : 'hover:cursor-grab! active:cursor-grabbing!'}`}
-                              >
-                                  {member.surname} {member.name}
-                              </button>
-                          </li>
-                      ))
-                : undefined}
-        </ul>
+                            >
+                                {member.surname} {member.name}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ) : undefined}
+            {ofLegalAgeMembersNoJob?.length ? (
+                <ul className="flex flex-wrap gap-x-4 gap-y-2 items-start content-start">
+                    {ofLegalAgeMembersNoJob.map((member) => (
+                        <li
+                            key={member.id}
+                            draggable={currentWorkersCount !== job.workers}
+                            onDragStart={(e) => handleDragStart(e, member.id)}
+                            onDragEnd={handleDragEnd}
+                        >
+                            <button
+                                disabled={currentWorkersCount === job.workers}
+                                onKeyDown={(e) =>
+                                    handleKeyDown(e, member, 'assign')
+                                }
+                                className={`primary-text-pseudo-tertiary bg-slate-800 text-white px-2 py-1 text-sm md:text-base select-none 
+                                                ${currentWorkersCount === job.workers ? '' : 'hover:cursor-grab! active:cursor-grabbing!'}`}
+                            >
+                                {member.surname} {member.name}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ) : undefined}
+        </div>
     ) : (
         <ul
             className={`flex flex-wrap gap-x-4 gap-y-2 items-start content-start overflow-y-auto h-32 p-2 flex-1 min-w-44 xs:min-w-64 rounded-md
-                                ${activeTargetZone === 'assign' ? 'outline-dashed outline-2 outline-slate-200 bg-slate-200 animate-pulse' : 'bg-slate-400'}`}
+                ${activeTargetZone === 'assign' ? 'outline-dashed outline-2 outline-slate-200 bg-slate-200 animate-pulse' : 'bg-slate-400'}`}
             onDragOver={handleDragOver}
             onDrop={(e) => handleDrop(e, 'assign')}
             aria-label="Dieser Aufgabe zugewiesene Mitglieder"
