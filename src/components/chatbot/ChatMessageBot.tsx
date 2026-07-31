94e38a938b5fa1d0065697d0fefd9ff73a85a6ba
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-31 14:01:38 +0200

    max-w

--- src/components/chatbot/ChatMessageBot.tsx
+++ src/components/chatbot/ChatMessageBot.tsx
@@ -2,7 +2,7 @@ import type { ChatMessageProps } from '../../types/types'
 
 const ChatMessageBot = ({ message }: ChatMessageProps) => {
     return (
-        <div className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-r-lg bg-zinc-200 text-indigo-800 p-2 w-fit">
+        <div className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-r-lg bg-zinc-200 text-indigo-800 p-2 max-w-11/12 sm:max-w-4/5 w-fit">
             {message}
         </div>
     )
--- src/components/chatbot/ChatMessageUser.tsx
+++ src/components/chatbot/ChatMessageUser.tsx
@@ -2,7 +2,7 @@ import type { ChatMessageProps } from '../../types/types'
 
 const ChatMessageUser = ({ message }: ChatMessageProps) => {
     return (
-        <div className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-l-lg bg-indigo-700 border-2 border-zinc-200 p-2 w-fit self-end">
+        <div className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-l-lg bg-indigo-700 border-2 border-zinc-200 p-2 max-w-11/12 sm:max-w-4/5 w-fit self-end">
             {message}
         </div>
     )
