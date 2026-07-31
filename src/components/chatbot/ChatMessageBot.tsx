61d4045ac857d9e403af7a0c4c9b4428a75e7618
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-31 14:31:26 +0200

    format bot output with remark und rehype

--- src/components/chatbot/ChatMessageBot.tsx
+++ src/components/chatbot/ChatMessageBot.tsx
@@ -1,9 +1,18 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 import type { ChatMessageProps } from '../../types/types'
 
 const ChatMessageBot = ({ message }: ChatMessageProps) => {
     return (
         <div className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-r-lg bg-zinc-200 text-indigo-800 p-2 max-w-11/12 sm:max-w-4/5 w-fit">
-            {message}
+            <ReactMarkdown
+                remarkPlugins={[remarkGfm, remarkMath]}
+                rehypePlugins={[rehypeKatex]}
+            >
+                {message}
+            </ReactMarkdown>
         </div>
     )
 }
