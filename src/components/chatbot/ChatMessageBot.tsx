1c0e84545f9d28731e2169caf67f43cc440b4750
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-08-02 20:53:02 +0200

    smaller font size

--- src/components/chatbot/ChatMessageBot.tsx
+++ src/components/chatbot/ChatMessageBot.tsx
@@ -8,14 +8,14 @@ import type { ChatMessageBotProps } from '../../types/types'
 const ChatMessageBot = ({ index, message, status }: ChatMessageBotProps) => {
     return status === 'isLoading' ? (
         <li
-            className="rounded-t-lg rounded-r-lg bg-zinc-200 max-w-32 w-full py-2 sn:py-3 flex justify-center items-center"
+            className="rounded-t-lg rounded-r-lg bg-zinc-200 max-w-32 w-full py-1.5 sm:py-2 flex justify-center items-center"
             aria-label="Dieter.ai denkt nach"
         >
             <FetchLoading theme="#372aac" />
         </li>
     ) : (
         <li
-            className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-r-lg bg-zinc-200 text-indigo-800 p-2 max-w-11/12 sm:max-w-4/5 w-fit"
+            className="whitespace-pre-wrap text-sm md:text-base rounded-t-lg rounded-r-lg bg-zinc-200 text-indigo-800 p-2 max-w-11/12 sm:max-w-4/5 w-fit"
             aria-label={`Dieter.ai ${index === 0 ? 'fragt' : 'antwortet'}:`}
         >
             <ReactMarkdown
--- src/components/chatbot/ChatMessageUser.tsx
+++ src/components/chatbot/ChatMessageUser.tsx
@@ -3,7 +3,7 @@ import type { ChatMessageUserProps } from '../../types/types'
 const ChatMessageUser = ({ message }: ChatMessageUserProps) => {
     return (
         <li
-            className="whitespace-pre-wrap text-base md:text-lg rounded-t-lg rounded-l-lg bg-indigo-700 border-2 border-zinc-200 p-2 max-w-11/12 sm:max-w-4/5 w-fit self-end"
+            className="whitespace-pre-wrap text-sm md:text-base rounded-t-lg rounded-l-lg bg-indigo-700 border-2 border-zinc-200 p-2 max-w-11/12 sm:max-w-4/5 w-fit self-end"
             aria-label={`Du fragst: ${message}`}
         >
             {message}
